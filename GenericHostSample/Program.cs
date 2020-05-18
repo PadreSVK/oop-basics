428ed141a76e699aa8a0b084966ca47f64cc0750
Patrik Svikruha <patrik.svikruha@gmail.sk>
2020-05-18 23:59:33 +0200

    add simple generic host sample

---
+++ GenericHostSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace GenericHostSample
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args)
+                .Build()
+                .RunAsync();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+          Host.CreateDefaultBuilder(args)
+              .ConfigureServices((hostContext, services) =>
+              {
+                  var configurationSection = hostContext.Configuration.GetSection("TestVariable").Value;
+                  services.AddHostedService<SimpleHostedService>();
+              })
+            .ConfigureLogging((hostContext, configLogging) =>
+            {
+                configLogging.AddConsole();
+            });
+    }
+}
---
+++ GenericHostSample/SimpleHostedService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GenericHostSample
+{
+    public class SimpleHostedService : IHostedService
+    {
+        private ILogger<SimpleHostedService> logger;
+
+        public SimpleHostedService(ILogger<SimpleHostedService> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Custom hosted service started");
+            while (true)
+            {
+                await Task.Delay(1000);
+                logger.LogInformation("while invoke");
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Custom hosted service stopped");
+            return Task.CompletedTask;
+        }
+    }
+}
